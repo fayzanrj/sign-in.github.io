08a893a05a2429671c774c6487cf96995b92290e
fayzanrj <121216897+fayzanrj@users.noreply.github.com>
2023-01-08 11:57:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+let usernameInput = document.getElementById('username');
+let password = document.getElementById('password')
+let label1 = document.getElementById('label1')
+let label2 = document.getElementById('label2')
+
+const labelTransition = () =>{
+    label1.style.marginTop = '0'
+    label1.style.marginLeft = '0.1rem'
+}
+const labelTransition2 = () =>{
+    label2.style.marginTop = '0'
+    label2.style.marginLeft = '0.1rem'
+}
+
+usernameInput.addEventListener('focusout' , ()=>{
+    if(usernameInput.value == ''){
+        label1.style.marginTop = '1.7rem'
+        label1.style.marginLeft = '0.5rem'
+    }
+})
+
+password.addEventListener('focusout' , ()=>{
+    if(password.value == ''){
+        label2.style.marginTop = '1.7rem'
+        label2.style.marginLeft = '0.5rem'
+    }
+})
+
+
+let signUpRedirectWidth = document.getElementById('sign-up-redirect');
+let logIn = document.getElementById('log-in-section');
+let signUp = document.getElementById('sign-up-section');
+
+signUpRedirectWidth.addEventListener('click' , ()=>{
+
+    logIn.style.padding = '3rem 0 ';
+    logIn.style.transform = 'TranslateX(15rem)';
+    logIn.style.width = '0';
+    logIn.style.display = 'fixed';
+    document.getElementById('forgot-pass').style.display = 'none'
+
+    document.getElementById('sign-up-btn').style.display = 'block'
+    document.getElementById('forgot-pass2').style.display = 'block'
+    signUp.style.width = '30rem';
+    signUp.style.padding = '3rem 5rem';
+    signUp.style.transform = 'Translate(0)';
+})
+
+if(signUpRedirectWidth.style.display == "none"){
+    location.reload();
+}
+let signUpRedirectHeight = document.getElementById('sign-up-redirect2')
+
+signUpRedirectHeight.addEventListener('click' , ()=>{
+    console.log('hi')
+    logIn.style.padding = '0 0 ';
+    logIn.style.transform = 'TranslateY(15rem)';
+    logIn.style.height = '0';
+    logIn.style.display = 'fixed';
+    document.getElementById('forgot-pass').style.display = 'none'
+
+    document.getElementById('sign-up-btn').style.display = 'block'
+    document.getElementById('forgot-pass2').style.display = 'block'
+    signUp.style.height = '25rem';
+    signUp.style.padding = '1rem 5rem';
+    signUp.style.transform = 'Translate(0)';
+})
+
+
+
+
+
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,82 @@
+
+let usernameInput2 = document.getElementById('username2');
+let password2 = document.getElementById('password2')
+let label1_1 = document.getElementById('label1-2')
+let label2_2 = document.getElementById('label2-2')
+let eMail = document.getElementById('e-mail');
+let emailLable = document.getElementById('e-mail_label');
+
+const labelTransition_1 = () =>{
+    label1_1.style.marginTop = '0'
+    label1_1.style.marginLeft = '0.1rem'
+}
+const labelTransition_2 = () =>{
+    label2_2.style.marginTop = '0'
+    label2_2.style.marginLeft = '0.1rem'
+}
+const labelTransition_3 = () =>{
+    emailLable.style.marginTop = '0'
+    emailLable.style.marginLeft = '0.1rem'
+}
+
+usernameInput2.addEventListener('focusout' , ()=>{
+    if(usernameInput2.value == ''){
+        label1_1.style.marginTop = '1.7rem'
+        label1_1.style.marginLeft = '0.5rem'
+    }
+})
+
+password2.addEventListener('focusout' , ()=>{
+    if(password2.value == ''){
+        label2_2.style.marginTop = '1.7rem'
+        label2_2.style.marginLeft = '0.5rem'
+    }
+})
+
+eMail.addEventListener('focusout' , ()=>{
+    if(eMail.value == ''){
+        emailLable.style.marginTop = '1.7rem'
+        emailLable.style.marginLeft = '0.5rem'
+    }
+})
+
+
+let signUpRedirectWidth2 = document.getElementById('sign-in-redirect');
+// let logIn = document.getElementById('log-in-section');
+// let signUp = document.getElementById('sign-up-section');
+
+signUpRedirectWidth2.addEventListener('click' , ()=>{
+    
+    signUp.style.padding = '3rem 0';
+    signUp.style.transform = 'TranslateX(-15rem)';
+    signUp.style.width = '0';
+    signUp.style.display = 'fixed';
+    document.getElementById('forgot-pass2').style.display = 'none'
+    document.getElementById('sign-up-btn').style.display = 'none'
+    
+    document.getElementById('forgot-pass').style.display = 'block'
+    logIn.style.width = '30rem';
+    logIn.style.transform = 'TranslateX(0)'
+    logIn.style.padding = '3rem 5rem';
+})
+
+let signInRedirectHeight2 = document.getElementById('sign-in-redirect2')
+
+
+signInRedirectHeight2.addEventListener('click' , ()=>{
+    signUp.style.padding = '0';
+    signUp.style.transform = 'TranslateY(-15rem)';
+    signUp.style.height = '0';
+    signUp.style.display = 'fixed';
+    document.getElementById('forgot-pass2').style.display = 'none'
+    document.getElementById('sign-up-btn').style.display = 'none'
+    
+    document.getElementById('forgot-pass').style.display = 'block'
+    logIn.style.height = '25rem';
+    logIn.style.transform = 'TranslateY(0)'
+    logIn.style.padding = '3rem 5rem';
+})
+
+
+
+
