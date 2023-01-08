168401dc7cf6379410c7f79329dad6cf8380b114
fayzanrj <121216897+fayzanrj@users.noreply.github.com>
2023-01-08 13:58:47 +0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -7,15 +7,15 @@ let eMail = document.getElementById('e-mail');
 let emailLable = document.getElementById('e-mail_label');
 
 const labelTransition_1 = () =>{
-    label1_1.style.marginTop = '0'
+    label1_1.style.marginTop = '0.5rem'
     label1_1.style.marginLeft = '0.1rem'
 }
 const labelTransition_2 = () =>{
-    label2_2.style.marginTop = '0'
+    label2_2.style.marginTop = '0.5rem'
     label2_2.style.marginLeft = '0.1rem'
 }
 const labelTransition_3 = () =>{
-    emailLable.style.marginTop = '0'
+    emailLable.style.marginTop = '0.5rem'
     emailLable.style.marginLeft = '0.1rem'
 }
 
@@ -54,6 +54,7 @@ signUpRedirectWidth2.addEventListener('click' , ()=>{
     document.getElementById('forgot-pass2').style.display = 'none'
     document.getElementById('sign-up-btn').style.display = 'none'
     
+    document.title = 'Sign In'
     document.getElementById('forgot-pass').style.display = 'block'
     logIn.style.width = '30rem';
     logIn.style.transform = 'TranslateX(0)'
@@ -64,19 +65,16 @@ let signInRedirectHeight2 = document.getElementById('sign-in-redirect2')
 
 
 signInRedirectHeight2.addEventListener('click' , ()=>{
-    signUp.style.padding = '0';
+    signUp.style.padding = '0 5rem';
     signUp.style.transform = 'TranslateY(-15rem)';
     signUp.style.height = '0';
     signUp.style.display = 'fixed';
     document.getElementById('forgot-pass2').style.display = 'none'
     document.getElementById('sign-up-btn').style.display = 'none'
     
+    document.title = 'Sign In'
     document.getElementById('forgot-pass').style.display = 'block'
     logIn.style.height = '25rem';
     logIn.style.transform = 'TranslateY(0)'
     logIn.style.padding = '3rem 5rem';
 })
-
-
-
-
